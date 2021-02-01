be20eb00d0592f7526511ae09fdfae577cff7fc9
zebrainncu <persimon.vitality@gmail.com>
2021-02-01 15:26:02 +0800

    Add files via upload

---
+++ SensorThingsServer.Core/src/main/java/de/fraunhofer/iosb/ilt/sta/path/EntityType.java
@@ -0,0 +1,205 @@
+/*
+ * Copyright (C) 2016 Fraunhofer Institut IOSB, Fraunhoferstr. 1, D 76131
+ * Karlsruhe, Germany.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.fraunhofer.iosb.ilt.sta.path;
+
+import de.fraunhofer.iosb.ilt.sta.model.*;
+import de.fraunhofer.iosb.ilt.sta.model.core.Entity;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ * @author jab, scf
+ */
+public enum EntityType {
+
+    Datastream("Datastreams", Datastream.class),
+    MultiDatastream("MultiDatastreams", MultiDatastream.class),
+    FeatureOfInterest("FeaturesOfInterest", FeatureOfInterest.class),
+    HistoricalLocation("HistoricalLocations", HistoricalLocation.class),
+    Location("Locations", Location.class),
+    Observation("Observations", Observation.class),
+    ObservedProperty("ObservedProperties", ObservedProperty.class),
+    Sensor("Sensors", Sensor.class),
+    Thing("Things", Thing.class),
+
+    TaskingCapability("TaskingCapabilities",TaskingCapability.class);//20170310 add
+
+    public static class PropertyEntry {
+
+        public final Property property;
+        /**
+         * Flag indicating the property must be set when creating an Entity.
+         */
+        public final boolean required;
+
+        public PropertyEntry(Property property, boolean required) {
+            this.property = property;
+            this.required = required;
+        }
+
+    }
+    /**
+     * The name of this entity type as used in URLs.
+     */
+    public final String name;
+    /**
+     * The name of collections of this entity type as used in URLs.
+     */
+    public final String plural;
+    /**
+     * The Set of Properties that Entities of this type have.
+     */
+    private final Map<Property, Boolean> propertySet = new HashMap<>();
+    private final Class<? extends Entity> implementingClass;
+
+    static {
+        Map<Property, Boolean> propertySet;
+        propertySet = Datastream.propertySet;
+        propertySet.put(EntityProperty.Id, false);
+        propertySet.put(EntityProperty.SelfLink, false);
+        propertySet.put(EntityProperty.Name, true);
+        propertySet.put(EntityProperty.Description, true);
+        propertySet.put(EntityProperty.ObservationType, true);
+        propertySet.put(EntityProperty.UnitOfMeasurement, true);
+        propertySet.put(EntityProperty.ObservedArea, false);
+        propertySet.put(EntityProperty.PhenomenonTime, false);
+        propertySet.put(EntityProperty.ResultTime, false);
+        propertySet.put(NavigationProperty.ObservedProperty, true);
+        propertySet.put(NavigationProperty.Sensor, true);
+        propertySet.put(NavigationProperty.Thing, true);
+        propertySet.put(NavigationProperty.Observations, false);
+
+        propertySet = MultiDatastream.propertySet;
+        propertySet.put(EntityProperty.Id, false);
+        propertySet.put(EntityProperty.SelfLink, false);
+        propertySet.put(EntityProperty.Name, true);
+        propertySet.put(EntityProperty.Description, true);
+        propertySet.put(EntityProperty.ObservationType, true);
+        propertySet.put(EntityProperty.MultiObservationDataTypes, true);
+        propertySet.put(EntityProperty.UnitOfMeasurements, true);
+        propertySet.put(EntityProperty.ObservedArea, false);
+        propertySet.put(EntityProperty.PhenomenonTime, false);
+        propertySet.put(EntityProperty.ResultTime, false);
+        propertySet.put(NavigationProperty.ObservedProperties, true);
+        propertySet.put(NavigationProperty.Sensor, true);
+        propertySet.put(NavigationProperty.Thing, true);
+        propertySet.put(NavigationProperty.Observations, false);
+
+        propertySet = FeatureOfInterest.propertySet;
+        propertySet.put(EntityProperty.Id, false);
+        propertySet.put(EntityProperty.SelfLink, false);
+        propertySet.put(EntityProperty.Name, true);
+        propertySet.put(EntityProperty.Description, true);
+        propertySet.put(EntityProperty.EncodingType, true);
+        propertySet.put(EntityProperty.Feature, true);
+        propertySet.put(NavigationProperty.Observations, false);
+
+        propertySet = HistoricalLocation.propertySet;
+        propertySet.put(EntityProperty.Id, false);
+        propertySet.put(EntityProperty.SelfLink, false);
+        propertySet.put(EntityProperty.Time, true);
+        propertySet.put(NavigationProperty.Thing, true);
+        propertySet.put(NavigationProperty.Locations, false);
+
+        propertySet = Location.propertySet;
+        propertySet.put(EntityProperty.Id, false);
+        propertySet.put(EntityProperty.SelfLink, false);
+        propertySet.put(EntityProperty.Name, true);
+        propertySet.put(EntityProperty.Description, true);
+        propertySet.put(EntityProperty.EncodingType, true);
+        propertySet.put(EntityProperty.Location, true);
+        propertySet.put(NavigationProperty.HistoricalLocations, false);
+        propertySet.put(NavigationProperty.Things, false);
+
+        propertySet = Observation.propertySet;
+        propertySet.put(EntityProperty.Id, false);
+        propertySet.put(EntityProperty.SelfLink, false);
+        propertySet.put(EntityProperty.PhenomenonTime, false);
+        propertySet.put(EntityProperty.ResultTime, false);
+        propertySet.put(EntityProperty.Result, true);
+        propertySet.put(EntityProperty.ResultQuality, false);
+        propertySet.put(EntityProperty.ValidTime, false);
+        propertySet.put(EntityProperty.Parameters, false);
+        // One of the following two is mandatory.
+        propertySet.put(NavigationProperty.Datastream, false);
+        propertySet.put(NavigationProperty.MultiDatastream, false);
+        // FeatureOfInterest must be generated on the fly if not present.
+        propertySet.put(NavigationProperty.FeatureOfInterest, false);
+
+        propertySet = ObservedProperty.propertySet;
+        propertySet.put(EntityProperty.Id, false);
+        propertySet.put(EntityProperty.SelfLink, false);
+        propertySet.put(EntityProperty.Name, true);
+        propertySet.put(EntityProperty.Definition, true);
+        propertySet.put(EntityProperty.Description, true);
+        propertySet.put(NavigationProperty.Datastreams, false);
+        propertySet.put(NavigationProperty.MultiDatastreams, false);
+
+        propertySet = Sensor.propertySet;
+        propertySet.put(EntityProperty.Id, false);
+        propertySet.put(EntityProperty.SelfLink, false);
+        propertySet.put(EntityProperty.Name, true);
+        propertySet.put(EntityProperty.Description, true);
+        propertySet.put(EntityProperty.EncodingType, true);
+        propertySet.put(EntityProperty.Metadata, true);
+        propertySet.put(NavigationProperty.Datastreams, false);
+        propertySet.put(NavigationProperty.MultiDatastreams, false);
+
+        propertySet = Thing.propertySet;
+        propertySet.put(EntityProperty.Id, false);
+        propertySet.put(EntityProperty.SelfLink, false);
+        propertySet.put(EntityProperty.Name, true);
+        propertySet.put(EntityProperty.Description, true);
+        propertySet.put(EntityProperty.Properties, false);
+        propertySet.put(NavigationProperty.Locations, false);
+        propertySet.put(NavigationProperty.HistoricalLocations, false);
+        propertySet.put(NavigationProperty.Datastreams, false);
+        propertySet.put(NavigationProperty.MultiDatastreams, false);
+    }
+
+    private EntityType(String plural, Class<? extends Entity> implementingClass) {
+        this.name = name();
+        this.plural = plural;
+        this.implementingClass = implementingClass;
+    }
+
+    /**
+     * @return The Set of Properties that Entities of this type have.
+     */
+    public Set<Property> getPropertySet() {
+        return propertySet.keySet();
+    }
+
+    /**
+     * @param property The property to check the required state for.
+     * @return True when the property is required, false otherwise.
+     */
+    public boolean isRequired(Property property) {
+        if (!propertySet.containsKey(property)) {
+            return false;
+        }
+        return propertySet.get(property);
+    }
+
+    public Class<? extends Entity> getImplementingClass() {
+        return implementingClass;
+    }
+
+}
---
+++ SensorThingsServer.Core/src/main/java/de/fraunhofer/iosb/ilt/sta/persistence/PersistenceManagerFactory.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2016 Fraunhofer IOSB
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.fraunhofer.iosb.ilt.sta.persistence;
+
+import de.fraunhofer.iosb.ilt.sta.settings.CoreSettings;
+import javax.swing.event.EventListenerList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author jab
+ */
+public class PersistenceManagerFactory {
+
+    private static final String ERROR_MSG = "Could not generate PersistenceManager instance: ";
+    private static final EventListenerList entityChangeListeners = new EventListenerList();
+    private static final Logger LOGGER = LoggerFactory.getLogger(PersistenceManagerFactory.class);
+    private static PersistenceManagerFactory instance;
+
+    public static synchronized void init(CoreSettings settings) {
+        if (instance == null) {
+            instance = new PersistenceManagerFactory(settings);
+        }
+    }
+
+    public static void addEntityChangeListener(EntityChangeListener listener) {
+        entityChangeListeners.add(EntityChangeListener.class, listener);
+    }
+
+    public static void removeEntityChangeListener(EntityChangeListener listener) {
+        entityChangeListeners.remove(EntityChangeListener.class, listener);
+    }
+
+    public static PersistenceManagerFactory getInstance() {
+        if (instance == null) {
+            throw new IllegalStateException("PersistanceManagerFactory is not initialized! Call init() before accessing the instance.");
+        }
+        return instance;
+    }
+    private final Class persistenceManagerClass;
+    private final CoreSettings settings;
+
+    private PersistenceManagerFactory(CoreSettings settings) {
+        if (settings == null) {
+            throw new IllegalArgumentException("settings must be non-null");
+        }
+        this.settings = settings;
+        try {
+            persistenceManagerClass = Class.forName(settings.getPersistenceSettings().getPersistenceManagerImplementationClass());
+        } catch (ClassNotFoundException ex) {
+            throw new IllegalArgumentException(ERROR_MSG + "Class '" + settings.getPersistenceSettings().getPersistenceManagerImplementationClass() + "' could not be found", ex);
+        }
+    }
+
+    public PersistenceManager create() {
+        PersistenceManager persistenceManager = null;
+        try {
+            persistenceManager = (PersistenceManager) persistenceManagerClass.newInstance();
+            persistenceManager.init(settings);
+            for (EntityChangeListener listener : entityChangeListeners.getListeners(EntityChangeListener.class)) {
+                persistenceManager.addEntityChangeListener(listener);
+            }
+        } catch (InstantiationException | IllegalAccessException ex) {
+            LOGGER.error(ERROR_MSG + "Class '" + settings.getPersistenceSettings().getPersistenceManagerImplementationClass() + "' could not be instantiated", ex);
+        }
+        return persistenceManager;
+    }
+}
